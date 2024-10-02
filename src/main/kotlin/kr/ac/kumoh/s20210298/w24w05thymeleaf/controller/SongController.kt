317a8aaf40e7cd4778f4219d09d9093b9be31409
20210298 <144835269+20210298@users.noreply.github.com>
2024-10-02 16:37:25 +0900

    💡 model 추가

--- src/main/kotlin/kr/ac/kumoh/s20210298/w24w05thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210298/w24w05thymeleaf/controller/SongController.kt
@@ -3,7 +3,8 @@ package kr.ac.kumoh.s20210298.w24w05thymeleaf.controller
 @Controller
 class SongController {
     @GetMapping("/song/random")
-    fun getRandomSong(): String {
+    fun getRandomSong(model: Model): String {
+        model.addAttribute("songTitle", "supernova")
         return "random"
     }
 }
